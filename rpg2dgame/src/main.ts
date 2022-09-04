8bb95dfee24284814e80a6d22cf7a0042c6d8645
alexis.h <alexis.h@medicaim.com>
2022-09-04 11:03:37 +0200

    (~) extract event handler management inside a class

--- rpg2dgame/src/main.ts
+++ rpg2dgame/src/main.ts
@@ -1,12 +1,11 @@
-import { textureMapper, firstMap } from './core/MapSystem/layerFloorWall';
-import Player from './core/entities/Player';
+import { textureMapper } from './core/MapSystem/layerFloorWall';
 import KeyboardEventHandler from './services/eventHandler/KeyboardEventHandler';
-import MobSpawner from './core/entities/MobSpawner';
 import renderer from './renderer/renderer';
 
-import C_CONFIG from './config/baseconfig';
 
+import globalGameObject from './core/globalGameObject/SingletonGlobalGameObject';
 import _GlobalGameObject from './core/globalGameObject/GlobalGameObject';
+
 import rendererDeathScreen from './renderer/rendererDeathScreen';
 import GameScreen from './core/entities/GameScreen'
 
@@ -62,55 +61,16 @@ function runGameLoop(ctx: CanvasRenderingContext2D, keyboardEvent: KeyboardEvent
   }
 }
 
-const initGlobalGameObject = (GlobalGameObject: _GlobalGameObject) => {
-  GlobalGameObject.player = new Player({ x: 5, y: 5 }, 'green', C_CONFIG.PLAYER.PLAYER_DMG, C_CONFIG.PLAYER.PLAYER_INIT_LIFE, C_CONFIG.PLAYER.PLAYER_MAX_LIFE, C_CONFIG.PLAYER.PLAYER_SKIN);
-
-  GlobalGameObject.map2d = firstMap.map2d;
-  // describe fov of map
-  GlobalGameObject._map2d.fieldOfView = C_CONFIG.GAME_CONFIG.FOV;
-
-  GlobalGameObject.addMobSpawner(new MobSpawner(2, { start: { x: 5, y: 5 }, end: { x: 10, y: 10 } }));
-  //GlobalGameObject.addMobSpawner(new MobSpawner(10, { start: { x: 10, y: 10 }, end: { x: 20, y: 20 } }));
-  //GlobalGameObject.addMobSpawner(new MobSpawner(10, { start: { x: 20, y: 10 }, end: { x: 25, y: 20 } }));
-
-
-  return GlobalGameObject;
-}
-
 export function initCanvas() {
   let canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  let GlobalGameObject = initGlobalGameObject(new _GlobalGameObject());
+  let GlobalGameObject = globalGameObject;//initGlobalGameObject(new _GlobalGameObject());
   
   GlobalGameObject.gameScreen = new GameScreen({ x: canvas.width, y: canvas.height });
 
   //
 
   let eventHandler = new KeyboardEventHandler();
-  // manage event handler
-  document.addEventListener("keydown", (event: KeyboardEvent) => {
-    let key = event.key;
-
-    switch (key) {
-      case 'ArrowUp':
-        eventHandler.dir = 'TOP';
-        break;
-      case 'ArrowDown':
-        eventHandler.dir = 'BOTTOM';
-
-        break;
-      case 'ArrowLeft':
-        eventHandler.dir = 'LEFT';
-
-        break;
-      case 'ArrowRight':
-        eventHandler.dir = 'RIGHT';
-
-        break;
-    }
-  })
-  document.addEventListener("keyup", () => {
-    eventHandler.dir = 'NONE';
-  })
+  eventHandler.init();
 
 
   if (!canvas) {
--- rpg2dgame/src/services/eventHandler/KeyboardEventHandler.ts
+++ rpg2dgame/src/services/eventHandler/KeyboardEventHandler.ts
@@ -6,6 +6,34 @@ class KeyboardEventHandler {
     constructor () {
       this._dir = 'NONE'
     }
+
+    public init () {
+      document.addEventListener("keydown", (event: KeyboardEvent) => {
+        let key = event.key;
+    
+        switch (key) {
+          case 'ArrowUp':
+            this._dir = 'TOP';
+            break;
+          case 'ArrowDown':
+            this._dir= 'BOTTOM';
+            break;
+          case 'ArrowLeft':
+            this._dir='LEFT';
+            break;
+          case 'ArrowRight':
+            this._dir= 'RIGHT';
+            break;
+        }
+      })
+      document.addEventListener("keyup", () => {
+        this._dir= 'NONE';
+      })
+    }
+
+    public destroy () {
+      
+    }
   
     get dir() : typesBase.T_DIR { return this._dir}
     set dir(dir : typesBase.T_DIR) {this._dir = dir}
